ad241f024b515f0be77458a1e8893c1538feb956
AmberMcVean <anm508@nyu.edu>
2019-11-23 21:05:01 -0500

    started coded with hashed passwords

---
+++ FinstaRough.py
@@ -0,0 +1,86 @@
+#Import Flask Library
+from flask import  Flask, render_template, request, session, url_for, redirect
+import pymysql.cursors
+import hashlib
+
+SALT = 'cs3083'
+
+#Step 1 make all the connections to mySQL
+conn = pymysql.connect(host='localhost',
+                       port=80,
+                       user='root',
+                       password='root',
+                       db='finstagram',
+                       charset='utf8mb4',
+                       cursorclass=pymysql.cursors.DictCursor)
+
+#Define a route to hello function-this is the first page the user/client sees
+@app.route('/')
+def hello():
+    return render_template('index.html')
+
+#Define route for login
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+#Define route for register
+@app.route('/register')
+def register():
+    return render_template('register.html')
+
+#Authenticates the login
+@app.route('/loginAuth', methods=['GET', 'POST'])
+def loginAuth():
+    #grabs information from the forms
+    username = request.form['username']
+    password = request.form['password'] + SALT
+    hashed_password = hashlib.sha256(password.encode('utf-8').hexdigest())
+
+    #cursor used to send queries
+    cursor = conn.cursor()
+    #executes query
+    query = 'SELECT * FROM user WHERE username = %s and password = %s'
+    cursor.execute(query, (username, hashed_password))
+    #stores the results in a variable
+    data = cursor.fetchone()
+    #use fetchall() if you are expecting more than 1 data row
+    cursor.close()
+    error = None
+    if(data):
+        #creates a session for the the user
+        #session is a built in
+        session['username'] = username
+        return redirect(url_for('home'))
+    else:
+        #returns an error message to the html page
+        error = 'Invalid login or username'
+        return render_template('login.html', error=error)
+
+    # Authenticates the register
+@app.route('/registerAuth', methods=['GET', 'POST'])
+def registerAuth():
+    # grabs information from the forms
+    username = request.form['username']
+    password = request.form['password'] + SALT
+    hashed_password = hashlib.sha256(password.encode('utf-8').hexdigest())
+
+    # cursor used to send queries
+    cursor = conn.cursor()
+    # executes query
+    query = 'SELECT * FROM user WHERE username = %s'
+    cursor.execute(query, (username))
+    # stores the results in a variable
+    data = cursor.fetchone()
+    # use fetchall() if you are expecting more than 1 data row
+    error = None
+    if (data):
+        # If the previous query returns data, then user exists
+        error = "This user already exists"
+        return render_template('register.html', error=error)
+    else:
+        ins = 'INSERT INTO user VALUES(%s, %s)'
+        cursor.execute(ins, (username, hashed_password))
+        conn.commit()
+        cursor.close()
+        return render_template('index.html')
